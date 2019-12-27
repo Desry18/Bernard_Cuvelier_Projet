0d317097be8e87b465b4db45c12043f283c6853a
Desry18 <56184354+Desry18@users.noreply.github.com>
2019-12-27 17:47:15 +0100

    DAO

--- workspace/Server/src/be/project/serv/AnimeApi.java
+++ workspace/Server/src/be/project/serv/AnimeApi.java
@@ -1,5 +1,79 @@
 package be.project.serv;
 
-public class AnimeApi {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-}
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import javax.ws.rs.core.Response.Status;
+
+import be.projet.dao.SingletonDB;
+import be.projet.pogo.Anime;
+
+
+@Path("anime")
+public class AnimeApi {
+	
+	@Path("all")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAllAnimeJson() throws SQLException {
+		
+		Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet resultado = null;
+        String querry = "SELECT titre_anime, studio_anime, nbr_episode FROM anime";
+    	List<Anime> listeAnime = new ArrayList<>();
+        SingletonDB dbt= new SingletonDB();
+        
+        try {
+            conn = dbt.getConnection();
+            statement = conn.prepareStatement(querry);
+            resultado = statement.executeQuery();
+            
+            
+            while (resultado.next()) {
+				String titre_anime = resultado.getString(1);
+				String studio_anime = resultado.getString(2);
+				int nbr_ep = resultado.getInt(3);
+				
+				listeAnime.add(new Anime(titre_anime, studio_anime, nbr_ep));
+
+        }
+        }catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if( resultado!= null) {
+				try {
+					resultado.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+        
+	
+
+		return Response .status(Status.OK)
+				.entity(listeAnime)
+				.build();
+		}
+	}
+
+
--- workspace/Server/src/be/project/serv/UtilApi.java
+++ workspace/Server/src/be/project/serv/UtilApi.java
@@ -1,5 +1,79 @@
 package be.project.serv;
 
-public class UtilApi {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-}
+import javax.ws.rs.core.Response.Status;
+
+import be.projet.dao.SingletonDB;
+import be.projet.pogo.Utilisateur;
+
+
+@Path("utilisateur")
+public class UtilApi {
+	
+	@Path("all")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAllUtilJson() throws SQLException {
+		
+		Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet resultado = null;
+        String querry = "SELECT email, pseudo, nom_util, mdp FROM utilisateur";
+    	List<Utilisateur> listeUtil = new ArrayList<>();
+        SingletonDB dbt= new SingletonDB();
+        
+        try {
+            conn = dbt.getConnection();
+            statement = conn.prepareStatement(querry);
+            resultado = statement.executeQuery();
+            
+            
+            while (resultado.next()) {
+				String email = resultado.getString(1);
+				String pseudo = resultado.getString(2);
+				String mdp = resultado.getString(4);
+				String nom = resultado.getString(3);
+				
+				listeUtil.add(new Utilisateur(pseudo, nom, mdp, email));
+
+        }
+        }catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if( resultado!= null) {
+				try {
+					resultado.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+        
+	
+
+		return Response .status(Status.OK)
+				.entity(listeUtil)
+				.build();
+		}
+	}
+
+
